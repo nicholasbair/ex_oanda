6f10f0c2a00c05a6cb6f8078f178ef6c4aabeebb
Nick Bair <bairinc@gmail.com>
2025-04-20 09:22:22 -0600

    Remove closeouts from validation (#51)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExOanda.MixProject do
   def project do
     [
       app: :ex_oanda,
-      version: "0.0.13",
+      version: "0.0.14",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
