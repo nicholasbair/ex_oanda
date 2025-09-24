976e26fed87877910dcdf73511029657dbe47501
Nick Bair <bairinc@gmail.com>
2025-09-24 17:35:47 -0600

    Add missing transaction types for streaming API (#68)

---
+++ lib/models/definitions/transaction/reopen_transaction.ex
@@ -0,0 +1,29 @@
+defmodule ExOanda.ReopenTransaction do
+  @moduledoc """
+  Schema for Oanda market order transaction.
+
+  [Oanda Docs](https://developer.oanda.com/rest-live-v20/transaction-df/)
+  """
+
+  use TypedEctoSchema
+  import Ecto.Changeset
+  alias ExOanda.Type.Atom
+
+  @primary_key false
+
+  typed_embedded_schema do
+    field(:id, :string)
+    field(:time, :utc_datetime_usec)
+    field(:user_id, :integer)
+    field(:account_id, :string)
+    field(:batch_id, :string)
+    field(:request_id, :string)
+    field(:type, Atom, default: :REOPEN)
+  end
+
+  @doc false
+  def changeset(struct, params) do
+    struct
+    |> cast(params, [:id, :time, :user_id, :account_id, :batch_id, :request_id, :type])
+  end
+end
