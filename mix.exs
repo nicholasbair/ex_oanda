9da3301bf94866399c9fc775e8b6166eeb8fad98
Nick Bair <bairinc@gmail.com>
2025-10-22 15:48:38 -0600

    Release prep (#104)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExOanda.MixProject do
   def project do
     [
       app: :ex_oanda,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: "Unofficial Elixir SDK for the Oanda API.",
